4870a5d12ee9682b3ff5310edf507f09faeb1aa8
noelnimstad <noel@nimstad.com>
2025-10-24 12:41:44 +0200

    circle drawing

--- source/main.c
+++ source/main.c
@@ -6,7 +6,7 @@ int main(void)
 {
     srand(100);
 
-    struct Anders *a = Anders_Initialise("render/", 400, 300, 60);
+    struct Anders *a = Anders_Initialise("render/", 400, 300, 120);
     if(NULL == a)
     {
         return 1;
@@ -14,27 +14,11 @@ int main(void)
 
     for(size_t i = 0; i < 100; i++)
     {
-        Anders_Palette_Clear(a, Anders_Palette_ColorLerp(GooglePalette[GOOGLE_BLUE_MEDIUM], GooglePalette[GOOGLE_GREEN_MEDIUM], (float)i / 100));
-        // Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
-        Anders_Frame(a);
-    }
-
-    for(size_t i = 0; i < 100; i++)
-    {
-        Anders_Palette_Clear(a, Anders_Palette_ColorLerp(GooglePalette[GOOGLE_GREEN_MEDIUM], GooglePalette[GOOGLE_YELLOW_MEDIUM], (float)i / 100));
-        // Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
-        Anders_Frame(a);
-    }
-
-    for(size_t i = 0; i < 100; i++)
-    {
-        Anders_Palette_Clear(a, Anders_Palette_ColorLerp(GooglePalette[GOOGLE_YELLOW_MEDIUM], GooglePalette[GOOGLE_RED_MEDIUM], (float)i / 100));
-        // Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
+        Anders_Palette_Circle(a, 200, 200, i + 1, GooglePalette[GOOGLE_BLUE_MEDIUM]);
         Anders_Frame(a);
     }
 
     Anders_Compose(a);
-    
     Anders_Destroy(a);
 
     return 0;
