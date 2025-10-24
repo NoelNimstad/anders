328e1f1f8163c878e38aa1f059afafd15f02f674
noelnimstad <noel@nimstad.com>
2025-10-24 11:42:12 +0200

    fixed color lerp macro

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 compile:
-	gcc source/anders/*.c source/*.c -o anders -I./ -ISDL -Wl,-rpath,SDL -FSDL -framework SDL3 -lz
+	gcc source/anders/*.c source/*.c -o anders
 
 ffmpeg:
 	ffmpeg -framerate 60 -i render/%08d.bmp -c:v libx264 -pix_fmt yuv420p output.mp4
--- source/main.c
+++ source/main.c
@@ -15,14 +15,21 @@ int main(void)
     for(size_t i = 0; i < 100; i++)
     {
         Anders_Palette_Clear(a, Anders_Palette_ColorLerp(GooglePalette[GOOGLE_BLUE_MEDIUM], GooglePalette[GOOGLE_GREEN_MEDIUM], (float)i / 100));
-        Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
+        // Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
         Anders_Frame(a);
     }
 
     for(size_t i = 0; i < 100; i++)
     {
-        Anders_Palette_Clear(a, Anders_Palette_ColorLerp(GooglePalette[GOOGLE_GREEN_MEDIUM], GooglePalette[GOOGLE_BLUE_MEDIUM], (float)i / 100));
-        Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
+        Anders_Palette_Clear(a, Anders_Palette_ColorLerp(GooglePalette[GOOGLE_GREEN_MEDIUM], GooglePalette[GOOGLE_YELLOW_MEDIUM], (float)i / 100));
+        // Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
+        Anders_Frame(a);
+    }
+
+    for(size_t i = 0; i < 100; i++)
+    {
+        Anders_Palette_Clear(a, Anders_Palette_ColorLerp(GooglePalette[GOOGLE_YELLOW_MEDIUM], GooglePalette[GOOGLE_RED_MEDIUM], (float)i / 100));
+        // Anders_Palette_Rectangle(a, 3, 3, 394, 294, Anders_Palette_MakeUniformColor(10));
         Anders_Frame(a);
     }
 
